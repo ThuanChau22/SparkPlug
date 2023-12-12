2f2eca2be36f5f30f521b61ec99c80138ca4f65d
rayraysheng <31055590+rayraysheng@users.noreply.github.com>
2023-12-12 04:52:22 -0800

    all stable

--- web-dashboard/src/pages/StationAnalytics.jsx
+++ web-dashboard/src/pages/StationAnalytics.jsx
@@ -3,20 +3,20 @@ import { apiInstance } from 'redux/api';
 import { CChart } from '@coreui/react-chartjs';
 import '../scss/StationAnalytics.scss';
 import StationAnalyticsModal from '../components/StationAnalyticsModal';
+import LocationFilter from '../components/LocationFilter';
 
 import { stationIcon } from '../components/mapIcons';
 import { MapContainer as LeafletMap, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import MapContainer from '../components/MapContainer';
+import StationMarker from '../components/StationMarker';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import StationMarker from '../components/StationMarker';
 
 const StationAnalytics = () => {
     const [stations, setStations] = useState([]);
     const [isAnalyticsModalOpen, setIsAnalyticsModalOpen] = useState(false);
     const [selectedStation, setSelectedStation] = useState(null);
     const [aggregateData, setAggregateData] = useState(null);
-
 
     const [filterState, setFilterState] = useState('all');
     const [filterCity, setFilterCity] = useState('all');
@@ -28,8 +28,7 @@ const StationAnalytics = () => {
     const [states, setStates] = useState([]);
     const [cities, setCities] = useState([]);
     const [zipCodes, setZipCodes] = useState([]);
-
-    const [analyticsData, setAnalyticsData] = useState(null);
+    const [filteredCities, setFilteredCities] = useState([]);
 
     const stationAPI = process.env.REACT_APP_STATION_API_ENDPOINT;
     const stationAnalyticsAPI = process.env.REACT_APP_STATION_ANALYTICS_API_ENDPOINT;
@@ -40,75 +39,51 @@ const StationAnalytics = () => {
                 const fetchedStations = response.data;
                 setStations(fetchedStations);
 
-                const uniqueStates = [...new Set(fetchedStations.map(station => station.state))];
-                const uniqueCities = [...new Set(fetchedStations.map(station => station.city))];
-                const uniqueZips = [...new Set(fetchedStations.map(station => station.zip_code))];
+                const uniqueStates = Array.from(new Set(fetchedStations.map(station => station.state))).sort((a, b) => a.localeCompare(b));
+                const uniqueCities = Array.from(new Set(fetchedStations.map(station => station.city))).sort((a, b) => a.localeCompare(b));
+                const uniqueZips = Array.from(new Set(fetchedStations.map(station => station.zip_code))).sort((a, b) => a.localeCompare(b));
 
                 setStates(['all', ...uniqueStates]);
                 setCities(['all', ...uniqueCities]);
                 setZipCodes(['all', ...uniqueZips]);
+                setFilteredCities(['all', ...uniqueCities]);
             })
             .catch(error => console.error('Error:', error));
 
         fetchAggregateData();
     }, []);
 
-    const formatDate = (dateStr) => {
-        if (!dateStr) return '';
-        const date = new Date(dateStr);
-        let month = '' + (date.getMonth() + 1);
-        let day = '' + date.getDate();
-        const year = date.getFullYear();
-
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-
-        return [month, day, year].join('/');
-    };
-
-    const fetchAggregateData = () => {
-        // Construct query parameters based on filters
+    const applyFilters = (state, city, zip) => {
         let queryParams = [];
-        if (startDate) queryParams.push(`start_date=${formatDate(startDate)}`);
-        if (endDate) queryParams.push(`end_date=${formatDate(endDate)}`);
-        if (chargeLevel !== 'all') queryParams.push(`charge_level=${chargeLevel}`);
-        let query = stationAnalyticsAPI;
-        if (queryParams.length > 0) query += '?' + queryParams.join('&');
+        if (zip !== 'all') {
+            queryParams.push(`zip=${encodeURIComponent(zip)}`);
+        } else {
+            if (state !== 'all') {
+                queryParams.push(`state=${encodeURIComponent(state)}`);
+            }
+            if (city !== 'all') {
+                queryParams.push(`city=${encodeURIComponent(city)}`);
+            }
+        }
 
-        apiInstance.get(query)
+        const queryString = queryParams.length > 0 ? `?${queryParams.join('&')}` : '';
+        apiInstance.get(`${stationAPI}${queryString}`)
             .then(response => {
-                setAggregateData(response.data); // Set the aggregateData state with the response
+                setStations(response.data);
             })
             .catch(error => console.error('Error:', error));
     };
 
-    const updateAnalyticsData = () => {
-        // Trigger useEffect to refetch data
-        setAnalyticsData(null); // Clear existing data
-        // useEffect will automatically be called since dependencies have changed
+    const onFiltersChange = (newState, newCity, newZip) => {
+        setFilterState(newState);
+        setFilterCity(newCity);
+        setFilterZip(newZip);
+        applyFilters(newState, newCity, newZip);
     };
 
     const handleStationClick = (stationId) => {
         setSelectedStation(stationId);
         setIsAnalyticsModalOpen(true);
-    };
-
-    const applyFilters = () => {
-        //let query = process.env.REACT_APP_
-        let query = stationAPI;
-        let queryParams = [];
-        if (filterState !== 'all') queryParams.push(`state=${filterState}`);
-        if (filterCity !== 'all') queryParams.push(`city=${filterCity}`);
-        if (filterZip !== 'all') queryParams.push(`zip=${filterZip}`);
-        if (queryParams.length > 0) {
-            query += '?' + queryParams.join('&');
-        }
-
-        apiInstance.get(query)
-            .then(response => {
-                setStations(response.data);
-            })
-            .catch(error => console.error('Error:', error));
     };
 
     const renderStationMarker = station => (
@@ -120,31 +95,31 @@ const StationAnalytics = () => {
         />
     );
 
+    const fetchAggregateData = () => {
+        let queryParams = [];
+        if (startDate) queryParams.push(`start_date=${startDate}`);
+        if (endDate) queryParams.push(`end_date=${endDate}`);
+        if (chargeLevel !== 'all') queryParams.push(`charge_level=${chargeLevel}`);
+
+        let query = `${stationAnalyticsAPI}?${queryParams.join('&')}`;
+        apiInstance.get(query)
+            .then(response => {
+                setAggregateData(response.data);
+            })
+            .catch(error => console.error('Error:', error));
+    };
+
     return (
         <div>
-            {/* Filter container for aggregate data */}
-            <div className="filter-container">
-                <select value={filterState} onChange={(e) => setFilterState(e.target.value)}>
-                    {states.map(state => (
-                        <option key={state} value={state}>{state}</option>
-                    ))}
-                </select>
-                <select value={filterCity} onChange={(e) => setFilterCity(e.target.value)}>
-                    {cities.map(city => (
-                        <option key={city} value={city}>{city}</option>
-                    ))}
-                </select>
-                <select value={filterZip} onChange={(e) => setFilterZip(e.target.value)}>
-                    {zipCodes.map(zip => (
-                        <option key={zip} value={zip}>{zip}</option>
-                    ))}
-                </select>
-                <button onClick={applyFilters}>Apply Filters</button>
-            </div>
+            <LocationFilter
+                states={states}
+                filteredCities={filteredCities}
+                zipCodes={zipCodes}
+                onFiltersChange={onFiltersChange}
+            />
 
             <MapContainer locations={stations} renderMarker={renderStationMarker} />
 
-            {/* Stations List */}
             <h2>Stations List</h2>
             <ul className="station-list">
                 {stations.map(station => (
@@ -154,14 +129,14 @@ const StationAnalytics = () => {
                 ))}
             </ul>
 
-            {/* Station Analytics Modal for individual station details */}
-            <StationAnalyticsModal
-                isOpen={isAnalyticsModalOpen}
-                onClose={() => setIsAnalyticsModalOpen(false)}
-                stationId={selectedStation}
-            />
+            {isAnalyticsModalOpen && (
+                <StationAnalyticsModal
+                    isOpen={isAnalyticsModalOpen}
+                    onClose={() => setIsAnalyticsModalOpen(false)}
+                    stationId={selectedStation}
+                />
+            )}
 
-            {/* Container for aggregate analytics charts and filters */}
             <div className="station-analytics-container">
                 <div className="analytics-filters">
                     <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
