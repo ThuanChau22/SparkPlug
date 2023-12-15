c356fa906d973acf19be18ae2b268ade46be578b
ThuanChau22 <thuan.q.chau@gmail.com>
2023-12-15 14:44:36 -0800

    refactor(web-dashboard): Restructure driver station view

--- web-dashboard/src/components/DriverStationModal.jsx
+++ web-dashboard/src/components/DriverStationModal.jsx
@@ -1,77 +1,133 @@
-import React, { useEffect, useState } from 'react';
-import { CChart } from '@coreui/react-chartjs';
-import { apiInstance } from 'redux/api';
-import '../scss/StationAnalyticsModal.scss';
+import { useCallback, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { GooeyCircleLoader } from "react-loaders-kit";
+import { CChart } from "@coreui/react-chartjs";
+import {
+  CContainer,
+  CForm,
+  CFormInput,
+  CFormSelect,
+  CInputGroup,
+  CInputGroupText,
+  CModal,
+  CModalHeader,
+  CModalTitle,
+  CModalBody,
+} from "@coreui/react";
 
-import stationIconUrl from '../assets/station_pointer.png';
+import { apiInstance } from "redux/api";
+import { selectAuthAccessToken } from "redux/auth/authSlice";
+import { selectStationById } from "redux/station/stationSlide";
 
 const DriverStationModal = ({ isOpen, onClose, stationId }) => {
-    const [analyticsData, setAnalyticsData] = useState(null);
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
-    const [chargeLevel, setChargeLevel] = useState('all');
+  const StationAnalyticsAPI = process.env.REACT_APP_STATION_ANALYTICS_API_ENDPOINT;
+  const station = useSelector((state) => selectStationById(state, stationId));
+  const token = useSelector(selectAuthAccessToken);
+  const [analyticsData, setAnalyticsData] = useState(null);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [chargeLevel, setChargeLevel] = useState("All");
 
-    const stationAnalyticsAPI = process.env.REACT_APP_STATION_ANALYTICS_API_ENDPOINT;
+  const formatDate = (dateStr) => {
+    if (!dateStr) return "";
+    const date = new Date(dateStr);
+    let month = "" + (date.getMonth() + 1);
+    let day = "" + date.getDate();
+    const year = date.getFullYear();
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+    return [month, day, year].join("/");
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, [stationId]);
-
-    const formatDate = (dateStr) => {
-        if (!dateStr) return '';
-        const date = new Date(dateStr);
-        let month = '' + (date.getMonth() + 1);
-        let day = '' + date.getDate();
-        const year = date.getFullYear();
-
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-
-        return [month, day, year].join('/');
-    };
-
-    const fetchData = () => {
-        if (stationId) {
-            let query = `${stationAnalyticsAPI}/${stationId}`;
-            let queryParams = [];
-            //if (startDate) queryParams.push(`start_date=${formatDate(startDate)}`);
-            //if (endDate) queryParams.push(`end_date=${formatDate(endDate)}`);
-            //if (chargeLevel !== 'all') queryParams.push(`charge_level=${chargeLevel}`);
-            if (queryParams.length > 0) query += '?' + queryParams.join('&');
-
-            apiInstance.get(query)
-                .then(response => {
-                    setAnalyticsData(response.data);
-                })
-                .catch(error => console.error('Error:', error));
-        }
-    };
-
-    if (!isOpen || !analyticsData) {
-        return null;
+  const fetchData = useCallback(async () => {
+    try {
+      const base = `${StationAnalyticsAPI}/${stationId}`;
+      const params = [];
+      if (startDate) params.push(`start_date=${formatDate(startDate)}`);
+      if (endDate) params.push(`end_date=${formatDate(endDate)}`);
+      if (chargeLevel !== "All") params.push(`charge_level=${chargeLevel}`);
+      const query = params.length > 0 ? `?${params.join("&")}` : "";
+      const headers = { Authorization: `Bearer ${token}` };
+      const { data } = await apiInstance.get(`${base}${query}`, { headers });
+      console.log(data);
+      setAnalyticsData(data);
+    } catch (error) {
+      console.log(error);
     }
+  }, [StationAnalyticsAPI, stationId, token, startDate, endDate, chargeLevel]);
 
-    const { peak_time } = analyticsData;
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
-    return (
-        <div className="modal-overlay">
-            <div className="modal-content">
-                <div className="analytics-filters">
-                    <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
-                    <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
-                    <select value={chargeLevel} onChange={(e) => setChargeLevel(e.target.value)}>
-                        <option value="all">All Levels</option>
-                        <option value="1">Level 1</option>
-                        <option value="2">Level 2</option>
-                        <option value="3">Level 3</option>
-                    </select>
-                    <button onClick={fetchData}>Update</button>
-                </div>
-                <CChart type="bar" data={peak_time} options={{ /* chart options here */ }} />
-                <button onClick={onClose}>Close</button>
-            </div>
-        </div>
-    );
+  return (
+    <CModal
+      size="xl"
+      alignment="center"
+      visible={isOpen}
+      onClose={onClose}
+      scrollable
+    >
+      <CModalHeader>
+        <CModalTitle>{station.name}</CModalTitle>
+      </CModalHeader>
+      <CForm className="d-flex align-item-center">
+        <CInputGroup>
+          <CInputGroupText className="bg-secondary text-white rounded-0">
+            From
+          </CInputGroupText>
+          <CFormInput
+            className="rounded-0 shadow-none"
+            type="date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+        </CInputGroup>
+        <CInputGroup>
+          <CInputGroupText className="bg-secondary text-white rounded-0">
+            To
+          </CInputGroupText>
+          <CFormInput
+            className="rounded-0 shadow-none"
+            type="date"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+          />
+        </CInputGroup>
+        <CInputGroup>
+          <CInputGroupText className="bg-secondary text-white rounded-0">
+            Charge Level
+          </CInputGroupText>
+          <CFormSelect
+            className="rounded-0 shadow-none"
+            options={[
+              { label: "All Levels", value: "All" },
+              { label: "Level 1", value: "1" },
+              { label: "Level 2", value: "2" },
+              { label: "Level 3", value: "3" },
+            ]}
+            value={chargeLevel}
+            onChange={(e) => setChargeLevel(e.target.value)}
+          />
+        </CInputGroup>
+      </CForm>
+      <CModalBody>
+        {analyticsData
+          ? (
+            <CChart type="bar" data={analyticsData.peak_time} />
+          )
+          : (
+            <CContainer className="d-flex flex-row justify-content-center">
+              <GooeyCircleLoader
+                className="mx-auto"
+                color={["#f6b93b", "#5e22f0", "#ef5777"]}
+                loading={true}
+              />
+            </CContainer>
+          )}
+      </CModalBody>
+    </CModal>
+  );
 };
 
 export default DriverStationModal;
