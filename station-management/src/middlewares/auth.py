39a10e46b75333ce14b20f3679ad7e10dca6132b
ThuanChau22 <thuan.q.chau@gmail.com>
2024-06-14 01:07:18 -0700

    feat(station-management): Handle auth on public endpoints

--- station-management/src/middlewares/auth.py
+++ station-management/src/middlewares/auth.py
@@ -10,19 +10,23 @@ def require_permission(*allowed_roles):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            if "Authorization" not in request.headers:
+            request.auth = {"role": "all"}
+            if "Authorization" in request.headers:
+                _, token = request.headers["Authorization"].split(" ")
+                body = {"token": token}
+                response = requests.post(f"{AUTH_API_ENDPOINT}/verify", json=body)
+                data = response.json()
+                if not response.status_code == 200:
+                    return data, response.status_code
+                if data["role"] not in allowed_roles:
+                    return {"message": "Permission denied"}, 403
+                request.auth = {
+                    "user_id": data["id"],
+                    "role": data["role"],
+                }
+            elif "all" not in allowed_roles:
                 return {"message": "Missing token"}, 401
-            _, token = request.headers["Authorization"].split(" ")
-            res = requests.post(f"{AUTH_API_ENDPOINT}/verify", json={"token": token})
-            data = res.json()
-            if not res.status_code == 200:
-                return data, res.status_code
-            if data["role"] not in allowed_roles:
-                return {"message": "Permission denied"}, 403
-            request.auth = {
-                "user_id": data["id"],
-                "role": data["role"],
-            }
+
             return f(*args, **kwargs)
 
         return decorated_function
