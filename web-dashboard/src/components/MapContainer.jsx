0ea292346c9474d4e1b75339cc4e7425d82b3008
ThuanChau22 <thuan.q.chau@gmail.com>
2024-08-04 03:56:58 -0700

    refactor(web-dashboard): Extract MapContent component from MapContainer

--- web-dashboard/src/components/MapContainer.jsx
+++ web-dashboard/src/components/MapContainer.jsx
@@ -1,81 +1,19 @@
-import { useEffect, useState } from "react";
 import {
   MapContainer as Map,
   TileLayer,
-  Marker,
-  Popup,
-  useMapEvents,
 } from "react-leaflet";
-import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-import { userIcon } from "assets/mapIcons";
+import MapContent from "components/MapContent";
 
-const MapContainer = ({
-  locations,
-  renderMarker,
-  setBound = true,
-  locate = false,
-  center = false,
-}) => {
-  const MapContent = () => {
-    const [position, setPosition] = useState(null);
-    const [isCenter, setIsCenter] = useState(center);
-    const map = useMapEvents({
-      locationfound({ latlng }) {
-        setPosition(latlng);
-        if (isCenter) {
-          map.flyTo(latlng, 14);
-          console.log("Fly");
-
-          setIsCenter(false);
-        }
-      },
-    });
-
-    useEffect(() => {
-      console.log({ isCenter });
-
-      if (locate && isCenter) {
-        console.log("Locate");
-
-        map.locate();
-      }
-    }, [map, isCenter]);
-
-    useEffect(() => {
-      if (setBound && locations && locations.length > 0) {
-        const coordinatePairs = locations.map((loc) => [loc.latitude, loc.longitude]);
-        map.fitBounds(new L.LatLngBounds(coordinatePairs));
-      }
-    }, [map]);
-
-    return (
-      <>
-        {locations.map((location) => renderMarker(location))}
-        {position && (
-          <Marker position={position} icon={userIcon} >
-            <Popup>You are here</Popup>
-          </Marker>
-        )}
-      </>
-    );
-  };
-
-  return (
-    <Map
-      center={[40, -100]}
-      zoom={5}
-      style={{ height: "100%", width: "100%" }}
-      placeholder={<noscript>You need to enable JavaScript to see this map.</noscript>}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-      />
-      <MapContent />
-    </Map>
-  );
-};
+const MapContainer = ({ positions = [], locate = false, children }) => (
+  <Map
+    style={{ height: "100%", width: "100%" }}
+    placeholder={<noscript>You need to enable JavaScript to see this map.</noscript>}
+  >
+    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    <MapContent positions={positions} locate={locate}>{children}</MapContent>
+  </Map>
+);
 
 export default MapContainer;
