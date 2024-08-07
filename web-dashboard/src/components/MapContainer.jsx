ec93d6d580e74b868d35f4f529cb0dcd259dd9ff
ThuanChau22 <thuan.q.chau@gmail.com>
2024-08-07 04:42:21 -0700

    style(web-dashboard): Attempt on dark theme for map

--- web-dashboard/src/components/MapContainer.jsx
+++ web-dashboard/src/components/MapContainer.jsx
@@ -11,7 +11,7 @@ const MapContainer = ({ positions = [], locate = false, children }) => (
     style={{ height: "100%", width: "100%" }}
     placeholder={<noscript>You need to enable JavaScript to see this map.</noscript>}
   >
-    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    <TileLayer className="map-tiles" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
     <MapContent positions={positions} locate={locate}>{children}</MapContent>
   </Map>
 );
