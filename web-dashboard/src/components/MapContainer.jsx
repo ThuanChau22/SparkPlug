850c7833f96c02ed635daaace50358b231e06704
ThuanChau22 <thuan.q.chau@gmail.com>
2024-08-09 08:26:59 -0700

    style(web-dashboard): Add default map view

--- web-dashboard/src/components/MapContainer.jsx
+++ web-dashboard/src/components/MapContainer.jsx
@@ -8,6 +8,8 @@ import MapContent from "components/MapContent";
 
 const MapContainer = ({ positions = [], locate = false, children }) => (
   <Map
+    center={[35, -115]}
+    zoom={6}
     style={{ height: "100%", width: "100%" }}
     placeholder={<noscript>You need to enable JavaScript to see this map.</noscript>}
   >
