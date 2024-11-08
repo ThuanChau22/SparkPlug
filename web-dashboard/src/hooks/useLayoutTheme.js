1b516c456b5d4304830b7cfaee4e27123b4955d9
ThuanChau22 <thuan.q.chau@gmail.com>
2024-11-08 00:01:53 -0800

    refactor(web-dashboard): Refactor useLayoutTheme hook

--- web-dashboard/src/hooks/useLayoutTheme.js
+++ web-dashboard/src/hooks/useLayoutTheme.js
@@ -3,24 +3,29 @@ import { useDispatch, useSelector } from "react-redux";
 import { useColorModes } from "@coreui/react";
 
 import {
+  ThemeModes,
   layoutStateSetTheme,
   selectLayoutTheme,
 } from "redux/layout/layoutSlice";
 
 const useLayoutTheme = () => {
-  const { colorMode, setColorMode } = useColorModes("theme");
+  const itemName = "theme";
+  const storedTheme = localStorage.getItem(itemName);
+  localStorage.setItem(itemName, storedTheme || ThemeModes.Auto);
+
+  const { setColorMode } = useColorModes(itemName);
   const theme = useSelector(selectLayoutTheme);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-    dispatch(layoutStateSetTheme(storedTheme || colorMode));
-  }, [colorMode, dispatch]);
+    const storedTheme = localStorage.getItem(itemName);
+    dispatch(layoutStateSetTheme(storedTheme));
+  }, [dispatch]);
 
   useEffect(() => {
     setColorMode(theme);
-    localStorage.setItem("theme", theme);
+    localStorage.setItem(itemName, theme);
   }, [theme, setColorMode]);
 
   return theme;
