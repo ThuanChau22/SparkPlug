c37b0de9add10888843ee838567af36474ae81dd
ThuanChau22 <thuan.q.chau@gmail.com>
2024-12-03 19:13:01 -0800

    refactor(web-dashboard): Combine MapResize into MapContainer

--- web-dashboard/src/components/Map/MapContainer.jsx
+++ web-dashboard/src/components/Map/MapContainer.jsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 import { MapContainer as Map, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-import MapResize from "components/Map/MapResize";
 import LoadingIndicator from "components/LoadingIndicator";
+import useWindowResize from "hooks/useWindowResize";
 
 const MapContainer = ({ loading = false, refHeight = 0, children }) => {
+  const mapRef = useRef();
+
   const [mapHeight, setMapHeight] = useState(0);
-  const onResize = () => setMapHeight(window.innerHeight - refHeight);
+  const [resize, setResize] = useState(false);
+
+  useWindowResize(useCallback(() => {
+    setMapHeight(window.innerHeight - refHeight);
+    setResize(true);
+  }, [refHeight]));
+
+  useEffect(() => {
+    if (resize) {
+      mapRef.current?.invalidateSize();
+      setResize(false);
+    }
+  }, [resize]);
+
   return (
     <div style={{ height: `${mapHeight}px` }}>
       <Map
+        ref={mapRef}
         style={{ height: "100%", width: "100%" }}
         bounceAtZoomLimits={true}
         center={[36, -119]}
@@ -27,7 +43,6 @@ const MapContainer = ({ loading = false, refHeight = 0, children }) => {
           maxZoom={21}
           updateWhenZooming={true}
         />
-        <MapResize onResize={onResize} />
         {children}
       </Map>
     </div>
