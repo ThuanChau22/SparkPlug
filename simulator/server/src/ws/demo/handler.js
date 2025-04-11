c21b567c5b86cd38791c54362d479efcc0007383
ThuanChau22 <thuan.q.chau@gmail.com>
2025-04-11 15:40:53 -0700

    fix(simulator-server): Prevent demo halt with fallback procedure on starting and stopping functions

--- simulator/server/src/ws/demo/handler.js
+++ simulator/server/src/ws/demo/handler.js
@@ -69,6 +69,7 @@ handler.simulate = (stationId, evseIds) => {
     }
 
     if (action === Simulator.DISCONNECT_CSMS && status === "Accepted") {
+      ws.close();
       stations.delete(stationId);
       for (const evseId of evseIds) {
         evses.delete(`${stationId},${evseId}`);
@@ -85,8 +86,15 @@ handler.simulate = (stationId, evseIds) => {
         }
       }
       server.wss.emit("progress");
-      ws.close();
     }
+
+    // console.log({
+    //   status: state.status,
+    //   totalCount: state.totalCount,
+    //   stations: state.stations.size,
+    //   evses: state.evses,
+    //   flowCtrl: state.flowCtrl,
+    // });
   });
 };
 
@@ -118,18 +126,18 @@ handler.starting = async (payload) => {
     const filtered = entries.filter(([_, value]) => value);
     const params = filtered.map(([key, value]) => `${key}=${value}`).join("&");
     const { data } = await axios.get(`${endpoint}${params ? `?${params}` : ""}`);
-    const { data: evses } = data;
-    state.totalCount = evses.length;
+    const { data: evseList } = data;
+    state.totalCount = evseList.length;
 
     const evsesByStations = new Map();
-    for (const { station_id, evse_id } of evses) {
+    for (const { station_id, evse_id } of evseList) {
       if (!evsesByStations.has(station_id)) {
         evsesByStations.set(station_id, []);
       }
       evsesByStations.get(station_id).push(evse_id);
     }
 
-    const { flowCtrl } = state;
+    const { stations, evses, flowCtrl } = state;
     const iterator = evsesByStations.entries();
     let entry = iterator.next();
     while (!entry.done && state.status === state.STARTING) {
@@ -147,6 +155,50 @@ handler.starting = async (payload) => {
       handler.simulate(stationId, evseIds);
 
       entry = iterator.next();
+    }
+
+    let retry = 3;
+    let currentSize = 0;
+    while (state.status === state.STARTING) {
+      if (currentSize !== evses.size) {
+        currentSize = evses.size;
+        await utils.sleep(ms("5s"));
+        continue;
+      }
+      if (evses.size !== state.totalCount && retry !== 0) {
+        retry--;
+        await utils.sleep(ms("5s"));
+        continue;
+      }
+      if (flowCtrl.queue.size !== 0) {
+        const remain = new Map();
+        for (const element of Array.from(flowCtrl.queue)) {
+          const [stationId, evseId] = element.split(",");
+          if (!remain.has(stationId)) {
+            remain.set(stationId, []);
+          }
+          remain.get(stationId).push(evseId);
+          flowCtrl.queue.delete(element);
+        }
+        for (const [stationId, evseIds] of remain.entries()) {
+          const socket = stations.get(stationId);
+          if (socket) {
+            for (const evseId of evseIds) {
+              const action = Simulator.PLUGIN_CABLE;
+              const payload = { evseId, connectorId: 1 };
+              socket.sendJson({ action, payload });
+            }
+          } else {
+            handler.simulate(stationId, evseIds);
+          }
+        }
+        retry = 3;
+        await utils.sleep(ms("5s"));
+        continue;
+      }
+      state.status = state.STARTED;
+      state.totalCount = evses.size;
+      server.wss.emit("progress");
     }
   } catch (error) {
     state.status = state.STARTED;
@@ -175,7 +227,7 @@ handler.stopping = async () => {
     state.status = state.STOPPING;
     server.wss.emit("progress");
 
-    const { stations, flowCtrl } = state;
+    const { stations, evses, flowCtrl } = state;
     const iterator = stations.entries();
     let entry = iterator.next();
     let isInitialized = false;
@@ -194,6 +246,42 @@ handler.stopping = async () => {
       socket.sendJson({ action, payload: {} });
 
       entry = iterator.next();
+    }
+
+    let retry = 3;
+    let currentSize = 0;
+    while (state.status === state.STOPPING) {
+      if (currentSize !== evses.size) {
+        currentSize = evses.size;
+        await utils.sleep(ms("5s"));
+        continue;
+      }
+      if (evses.size !== 0 && retry !== 0) {
+        retry--;
+        await utils.sleep(ms("5s"));
+        continue;
+      }
+      if (flowCtrl.queue.size !== 0) {
+        for (const element of Array.from(flowCtrl.queue)) {
+          if (Number.isInteger(element)) {
+            const socket = stations.get(element);
+            const action = Simulator.DISCONNECT_CSMS;
+            socket?.sendJson({ action, payload: {} });
+          }
+          flowCtrl.queue.delete(element);
+        }
+        retry = 3;
+        await utils.sleep(ms("5s"));
+        continue;
+      }
+      state.status = state.IDLE;
+      state.totalCount = 0;
+      for (const socket of stations.values()) {
+        socket.close();
+      }
+      stations.clear();
+      evses.clear();
+      server.wss.emit("progress");
     }
   } catch (error) {
     state.status = state.IDLE;
